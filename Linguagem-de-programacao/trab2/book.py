0dfc68f22ec27da27f1feaa15f5b7c8c149e6427
Caio Gabriel <caiogabriel123546@gmail.com>
2025-09-25 10:30:48 -0300

    feat: updating archives

--- Linguagem-de-programacao/trab2/book.py
+++ Linguagem-de-programacao/trab2/book.py
@@ -1,11 +1,5 @@
-"""
-Passo 1: Definir a classe Livro
-• Comece definindo a estrutura básica de um livro usando uma classe em Python. Cada livro
-terá atributos como título, autor, gênero e quantidade disponível.
-"""
-
 class Book:
-    """"Ola"""
+    """ Esta classe sera utilizada para instanciar objetos de um tipo personalizado """
     def __init__(self, title: str, author: str, genre: str, quantity: int):
         self.title = title
         self.author = author
--- Linguagem-de-programacao/trab2/main.py
+++ Linguagem-de-programacao/trab2/main.py
@@ -1,32 +1,23 @@
 """
-Passo 1: Definir a classe Livro
-• Comece definindo a estrutura básica de um livro usando uma classe em Python. Cada livro
-terá atributos como título, autor, gênero e quantidade disponível.
-
-Passo 2: Criar a lista de livros
-• Inicialize uma lista vazia para armazenar os livros que serão cadastrados.
-
-Passo 3: Implementar funções para gerenciar os livros
-• Função para cadastrar um novo livro
-• Função para listar todos os livros
-• Função para buscar um livro pelo título
-
-Passo 4: Utilizar a biblioteca Matplotlib para gerar um gráfico
-• Instalação da Matplotlib
-• Gerar o gráfico de quantidade de livros por gênero
+    Este programa e um sistema simples para o gerenciamento de livros
+    Ele permite cadastrar novos livros, listar todos os livros disponiveis, buscar
+    um livro pelo título, e gerar um gráfico com a quantidade de livros por genero.
 """
+#Importando a lib e a classe que serao utilizadas
 from matplotlib import pyplot as plt
 from book import Book
 
+#Lista para guardar os livros
 books = []
 
 def register_book():
-    """"Ola"""
+    """ Esta funcao permite que o usuario cadastre novos livros e guarda eles na lista"""
     title = input("Digite o titulo do livro: ")
     author = input("Digite o autor do livro: ")
     genre = input("Digite o genero do livro: ")
     quantity = int(input("Digite a quantidade de livros: "))
 
+    #Instanciando um livro com base nas informacoes inseridas
     book = Book(title, author, genre, quantity)
     books.append(book)
 
@@ -34,7 +25,8 @@ def register_book():
 
 
 def show_books():
-    """"Ola"""
+    """ Esta funcao imprime todos os livros da lista """
+    #Verifica se ha livros na lista
     if len(books) == 0:
         print("Nao ha nehum livro cadastrado.\n")
         return
@@ -48,25 +40,34 @@ def show_books():
 
 
 def find_book_by_title():
-    """"Ola"""
+    """ Esta funcao busca um livro pelo seu titulo """
+    #Verificando se ha livros na lista
     if len(books) == 0:
         print("Nao ha nehum livro cadastrado.\n")
         return
 
     title = input("Digite o titulo do livro para buscar: ")
 
+    #Iterando sobre a lista para buscar o livro
     for book in books:
         if book.title == title:
-            print(f"{book.title}, {book.author} ({book.genre}) quatidade: {book.quantity}.\n")
+            print(f"{book.title}, {book.author} ({book.genre}); quantidade: {book.quantity}.\n")
 
     print("Livro nao encontrado.")
 
 
 def show_graph():
-    """"Ola"""
+    """ Esta funcao exibe o grafico da quantidade de livros por genero"""
+    # Verificando se ha livros na lista
+    if len(books) == 0:
+        print("Nao ha nehum livro cadastrado.\n")
+        return
+
+    #Iterando sobre a lista para criar o grafico
     for book in books:
         plt.bar(book.genre, book.quantity, label=book.genre)
 
+    #Configurando o grafico
     plt.xlabel("Genero")
     plt.ylabel('Quantidade de livros')
     plt.title('Quantidade de livros por genero')
@@ -75,8 +76,10 @@ def show_graph():
     plt.show()
 
 def menu():
-    """Ola"""
+    """ Esta funcao exibe um menu interativo para o usuario utilizar as funcoes """
+    # Mantem o usuario no menu, ate que ele digite 0
     while True:
+        # Opcoes do menu
         print("1 - Cadastrar novo livro")
         print("2 - Mostrar todos os livros")
         print("3 - Buscar livro por titulo")
@@ -85,6 +88,7 @@ def menu():
 
         opt = int(input("Escolha uma das opcoes acima: "))
 
+        # Usando um match-case para enviar o usuario para a funcao escolhida
         match opt:
             case 1:
                 register_book()
@@ -94,7 +98,7 @@ def menu():
                 find_book_by_title()
             case 4:
                 show_graph()
-            case 0:
+            case 0: # Opcao de saida
                 print("Programa finalizado com sucesso.")
                 break
             case _:
